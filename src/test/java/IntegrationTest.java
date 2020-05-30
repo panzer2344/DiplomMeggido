bd57a3f5c4495f23819bc33ad111a2ba13af1b8a
panzer2344 <lexathebest23@gmail.com>
2020-05-30 23:27:20 +0300

    added some tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,15 +1,16 @@
-import com.sun.xml.internal.ws.util.StringUtils;
+import data.breeder.DataBreeder;
 import data.reader.DataReader;
+import model.Inequality;
 import model.LPTask;
 import org.javatuples.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 import solver.Solver2D;
+import solver.transformer.Transformer2D;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class IntegrationTest {
 
@@ -34,6 +35,34 @@ public class IntegrationTest {
     }
 
     @Test
+    public void test_with_breeder_x5() throws IOException, URISyntaxException {
+        String filename = "lp_task_test_data.txt";
+
+        LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
+
+        Pair<Double, Double> solution = new Solver2DWithBreed().solve(lpTask, 5, 0.5);
+
+        Pair<Double, Double> expected = new Pair<>(0.0, -1.0);
+
+        Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
+        Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    @Test
+    public void test_with_breeder_x6() throws IOException, URISyntaxException {
+        String filename = "lp_task_test_data.txt";
+
+        LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
+
+        Pair<Double, Double> solution = new Solver2DWithBreed().solve(lpTask, 6, 0.5);
+
+        Pair<Double, Double> expected = new Pair<>(0.0, -1.0);
+
+        Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
+        Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    @Test
     public void test1() throws IOException, URISyntaxException {
         String filename = "lp_task_test_data_1.txt";
         LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
@@ -44,6 +73,62 @@ public class IntegrationTest {
 
         Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
         Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    @Test
+    public void test1_with_breeder_x2() throws IOException, URISyntaxException {
+        String filename = "lp_task_test_data_1.txt";
+        LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
+
+        Pair<Double, Double> solution = new Solver2DWithBreed().solve(lpTask, 2, 0.5);
+
+        Pair<Double, Double> expected = new Pair<>(0.0, -1.0);
+
+        Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
+        Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    @Test
+    public void test1_with_breeder_x5() throws IOException, URISyntaxException {
+        String filename = "lp_task_test_data_1.txt";
+        LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
+
+        Pair<Double, Double> solution = new Solver2DWithBreed().solve(lpTask, 5, 0.5);
+
+        Pair<Double, Double> expected = new Pair<>(0.0, -1.0);
+
+        Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
+        Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    @Test
+    public void test1_with_breeder_x6() throws IOException, URISyntaxException {
+        String filename = "lp_task_test_data_1.txt";
+        LPTask lpTask = new DataReader().readLPTask(new DataReader().readStringFromClasspath(filename));
+
+        Pair<Double, Double> solution = new Solver2DWithBreed().solve(lpTask, 6, 0.5);
+
+        Pair<Double, Double> expected = new Pair<>(0.0, -1.0);
+
+        Assert.assertEquals(expected.getValue0(), solution.getValue0(), 0.0001);
+        Assert.assertEquals(expected.getValue1(), solution.getValue1(), 0.0001);
+    }
+
+    private class Solver2DWithBreed extends Solver2D {
+        private DataBreeder dataBreeder = new DataBreeder();
+
+        public Pair<Double, Double> solve(LPTask lpTask, int rate, double offset) {
+            Inequality[] inequalities = new Transformer2D().transform(lpTask);
+
+            System.out.println(Arrays.toString(inequalities));
+
+            Inequality[] breeded = dataBreeder.breed(inequalities, rate, offset);
+
+            System.out.println(Arrays.toString(breeded));
+
+            return solve(breeded);
+        }
+
     }
 
 }
